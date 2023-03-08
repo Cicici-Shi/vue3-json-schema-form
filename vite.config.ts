18038d6805a829d5bfdf8a273dbd64709a43fb2e
Cici <1901177100@qq.com>
2023-03-08 14:56:09 +0800

    feat basic

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,17 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import MonacoEditorPlugin from 'vite-plugin-monaco-editor'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    MonacoEditorPlugin({
+      languageWorkers: ['json']
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
